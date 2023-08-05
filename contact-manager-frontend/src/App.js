780f6c9ed2da0b9de01bae61eaae88b11a9c41ef
Amr Maket <amrmaket89@gmail.com>
2023-08-06 00:06:42 +0300

    installed react-leaflet

--- contact-manager-frontend/src/App.js
+++ contact-manager-frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './Header';
+import ContactForm from './ContactForm';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <ContactForm />
     </div>
   );
 }
--- contact-manager-frontend/src/ContactForm.js
+++ contact-manager-frontend/src/ContactForm.js
@@ -1,55 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './styles.css';
+
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+   integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+   crossorigin=""/>;
+
+<script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+   integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+   crossorigin=""></script>;
 
 const ContactForm = () => {
-    state = {
-        name : "",
-        phonenumber : "",
-        address : ""
-    };
-    add = (e) => {
+
+    const [name, setName] = useState("");
+    const [phonenumber, setPhoneNumber] = useState("");
+    const [address, setAddress] = useState("");
+    const position = [51.505, -0.09]
+
+
+    const add = (e) => {
         e.preventDefault();
         if(this.state.name === "" || this.state.phonenumber === "" || this.state.address === ""){
             alert("All fields are mandatory!");
             return;
         }
     };
-    render() {
-        return ( 
-            <div className="ui main">
-                <h2>Add Contact</h2>
-                <form className='ui main' onSubmit={ this.add }>
-                    <div className='field'>
-                        <label>Name</label>
-                        <input 
-                        type ='text'
-                        name = 'name'
-                        placeholder='Name'
-                        onChange={(e) => this.setState ({ name : e.target.value})} 
-                        />
-                    </div>
-                    <div className='field'>
-                        <label>Phone Number</label>
-                        <input 
-                        type ='text'
-                        name = 'phonenumber'
-                        placeholder='Phone Number'
-                        onChange={(e) => this.setState ({ phonenumber : e.target.value})} 
-                        />
-                    </div>
-                    <div className='field'>
-                        <label>Address</label>
-                        <input 
-                        type =''
-                        name = 'phonenumber'
-                        placeholder='Phone Number'
-                        onChange={(e) => this.setState ({ phonenumber : e.target.value})} 
-                        />
-                    </div>
-                </form>
-            </div>
-        );
-    }
+    return ( 
+        <div className="ui main">
+            <h2>Add Contact</h2>
+            <form className='ui main' onSubmit={ add }>
+                <div className='field'>
+                    <label>Name</label>
+                    <input 
+                    type ='text'
+                    name = 'name'
+                    placeholder='Name'
+                    onChange={(e) => setName(e.target.value)}
+                    />
+                </div>
+                <div className='field'>
+                    <label>Phone Number</label>
+                    <input 
+                    type ='text'
+                    name = 'phonenumber'
+                    placeholder='Phone Number'
+                    value={name}
+                    onChange={(e) => setPhoneNumber(e.target.value)}
+                    />
+                </div>
+                <div className='map'>
+        
+        render(
+          <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={position}>
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker>
+          </MapContainer>
+        )        
+                </div>
+            </form>
+        </div>
+    );
 }
- 
+
 export default ContactForm;
 
