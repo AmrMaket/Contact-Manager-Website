dc6aa17c63a88eeef7e1a400c171a245fcd2051f
Amr Maket <amrmaket89@gmail.com>
2023-08-05 17:22:21 +0300

    added ContactForm events

--- contact-manager-frontend/src/ContactForm.js
+++ contact-manager-frontend/src/ContactForm.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const  ContactForm = () => {
-    return (  );
+const ContactForm = () => {
+
+    add = (e) => {
+        e.preventDefault();
+        if(this.state.name === "" || this.state.phone_number === "" || this.state.address === ""){
+            alert("All fields are mandatory!");
+            return;
+        }
+    }
+
+    return ( 
+        <div className="ui main">
+            <h2>Add Contact</h2>
+            <form className='ui main' onSubmit={ this.add }></form>
+        </div>
+     );
 }
  
 export default ContactForm;
