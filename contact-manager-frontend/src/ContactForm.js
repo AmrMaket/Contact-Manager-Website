b72bf930863be5d66c1525753578d6b1bbb82a67
Amr Maket <amrmaket89@gmail.com>
2023-08-05 17:44:02 +0300

    added name and phone inputs

--- contact-manager-frontend/src/ContactForm.js
+++ contact-manager-frontend/src/ContactForm.js
@@ -1,21 +1,54 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 const ContactForm = () => {
-
+    state = {
+        name : "",
+        phonenumber : "",
+        address : ""
+    };
     add = (e) => {
         e.preventDefault();
-        if(this.state.name === "" || this.state.phone_number === "" || this.state.address === ""){
+        if(this.state.name === "" || this.state.phonenumber === "" || this.state.address === ""){
             alert("All fields are mandatory!");
             return;
         }
+    };
+    render() {
+        return ( 
+            <div className="ui main">
+                <h2>Add Contact</h2>
+                <form className='ui main' onSubmit={ this.add }>
+                    <div className='field'>
+                        <label>Name</label>
+                        <input 
+                        type ='text'
+                        name = 'name'
+                        placeholder='Name'
+                        onChange={(e) => this.setState ({ name : e.target.value})} 
+                        />
+                    </div>
+                    <div className='field'>
+                        <label>Phone Number</label>
+                        <input 
+                        type ='text'
+                        name = 'phonenumber'
+                        placeholder='Phone Number'
+                        onChange={(e) => this.setState ({ phonenumber : e.target.value})} 
+                        />
+                    </div>
+                    <div className='field'>
+                        <label>Address</label>
+                        <input 
+                        type =''
+                        name = 'phonenumber'
+                        placeholder='Phone Number'
+                        onChange={(e) => this.setState ({ phonenumber : e.target.value})} 
+                        />
+                    </div>
+                </form>
+            </div>
+        );
     }
-
-    return ( 
-        <div className="ui main">
-            <h2>Add Contact</h2>
-            <form className='ui main' onSubmit={ this.add }></form>
-        </div>
-     );
 }
  
 export default ContactForm;
