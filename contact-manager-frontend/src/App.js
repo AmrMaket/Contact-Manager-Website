8d8e4e595844f827d9bd394e220ccc07aecbe957
Amr Maket <amrmaket89@gmail.com>
2023-08-06 19:08:05 +0300

    added contactForm

--- contact-manager-frontend/src/App.js
+++ contact-manager-frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header/Header';
+import ContactForm from './components/ContactForm/ContactForm';
 
  
 function App() {
   return (
     <div className="App">
       <Header />
+      <ContactForm />
     </div>
   );
 }
