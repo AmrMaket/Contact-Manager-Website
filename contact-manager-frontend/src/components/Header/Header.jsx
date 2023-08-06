629dfb28a921ce38a86cd28decf4bf0a89f402f2
Amr Maket <amrmaket89@gmail.com>
2023-08-06 18:33:43 +0300

    added header with style begin with contact form

--- contact-manager-frontend/src/components/Header/Header.jsx
+++ contact-manager-frontend/src/components/Header/Header.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import "./Header.css";
+// import {Link, useNavigate} from "react-router-dom"
+
 
 const Header = () => {
     return ( 
-        <>
-        </>
+        <div className='navbar'>
+            <div className='logo'>
+                {/* <img src="../../assets/logo.jpg" alt="logo" className="logo"/> */}
+                <h1>Contact Manager</h1>
+            </div>
+            <a href='./ContactCard'>View All Contacts</a>
+            <a href='./ContactForm'>Create Contacts</a>
+            <a href='./map'>Map</a>
+        </div>
      );
 }
  
