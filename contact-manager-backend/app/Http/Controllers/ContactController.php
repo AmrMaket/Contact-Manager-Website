8ed28e40db96c09cc1f83866aef18efe337aae89
Amr Maket <amrmaket89@gmail.com>
2023-08-06 10:11:03 +0300

    added contactController

---
+++ contact-manager-backend/app/Http/Controllers/ContactController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace app\Http\Controllers ;
+
+use Illuminate\Http\Request;
+use App\Models\Contact;
+
+class ContactControlllers extends Controller {
+
+    public function addOrUpdateContact(Request $request, $id = "add"){
+        if($id == "add"){
+            $contact = new contact;
+        }else{
+            $contact = Contact :: find($id);
+        }
+
+        $contact->id = $request->$id;
+        $contact->name = $request->$name ? $request->name : $contact->name;
+        $contact->phonenumber = $request->$phonenumber ? $request->phonenumber : $contact->phonenumber;
+        $contact->longitude = $request->$longitude ? $request->longitude : $contact->longitude;
+        $contact->latitude = $request->$latitude ? $request->latitude: $contact->latitude;
+        $contact->save();
+
+        return response(["contacts" => $contact]);
+    }
+}
