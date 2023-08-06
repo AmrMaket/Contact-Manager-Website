27dd285b259a742d2da8392d2f491cceffca2b80
Amr Maket <amrmaket89@gmail.com>
2023-08-06 14:30:10 +0300

    fixed backend

--- contact-manager-backend/app/Http/Controllers/ContactController.php
+++ contact-manager-backend/app/Http/Controllers/ContactController.php
@@ -1,33 +1,32 @@
 <?php
 
-namespace app\Http\Controllers ;
+namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Contact;
 
-class ContactControlllers extends Controller {
-
+class ContactController extends Controller
+{
     public function addOrUpdateContact(Request $request, $id = "add"){
         if($id == "add"){
-            $contact = new contact;
+            $contact = new Contact;
         }else{
-            $contact = Contact :: find($id);
+            $contact = Contact::find($id);
         }
 
-        $contact->id = $request->$id;
-        $contact->name = $request->$name ? $request->name : $contact->name;
-        $contact->phonenumber = $request->$phonenumber ? $request->phonenumber : $contact->phonenumber;
-        $contact->longitude = $request->$longitude ? $request->longitude : $contact->longitude;
-        $contact->latitude = $request->$latitude ? $request->latitude: $contact->latitude;
+        $contact->id = $request-> id;
+        $contact->name = $request->name ? $request->name : $contact->name;
+        $contact->phone_number = $request->phone_number ? $request->phone_number : $contact->phone_number;
+        $contact->latitude = $request->latitude ? $request->latitude : $contact->latitude;
+        $contact->longitude = $request->longitude ? $request->longitude : $contact->longitude;
         $contact->save();
 
         return response(["contacts" => $contact]);
+
     }
 
-    public function getContacts()
-    {
+    function getContacts()
+    {   
         $contacts = Contact::all();
-
         return response(["contacts" => $contacts]);
     }
 }
--- contact-manager-backend/routes/api.php
+++ contact-manager-backend/routes/api.php
@@ -2,9 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Models\Contact;
-use App\Http\Controllers\ContactController;
+use app\Models\Contact;
+use app\Http\Controllers\ContactController;
 
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
 
 Route::post('/add_update_contact/{id?}', [ContactController::class, "addOrUpdateContact"]);
 Route::get('/show_contacts', [ContactController::class, "getContacts"]);
